02efe948eb2e9271575f02f3af37eed4fe22f556
Ethan Reed McElree <ethan.mcelree@gmail.com>
2022-10-10 16:26:23 -0500

    Doing contribution 3.

--- tests/codewars/TestFindOdd.java
+++ tests/codewars/TestFindOdd.java
@@ -18,5 +18,4 @@ public class TestFindOdd {
         assertEquals(10, FindOdd.findIt(new int[]{5,4,3,2,1,5,4,3,2,10,10,1,10}));
         assertEquals(-2, FindOdd.findIt(new int[]{2,1,-1,2,-2,5,-2,4,4,-1,-2,5,1}));
     }
-
 }
