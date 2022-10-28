cb22c374ffa13d75a047975d53a4ad177a164df9
Marwa Ibrahim <k0826719@student.kirkwood.edu>
2022-10-28 12:19:37 -0500

    Syncing project

---
+++ src/codewars/Kata.java
@@ -0,0 +1,9 @@
+package codewars;
+
+public class Kata {
+    public static String getBestCombination(int time) {
+        //your code
+        //String result = getBestCombination(71);
+        return "";
+    }
+}
