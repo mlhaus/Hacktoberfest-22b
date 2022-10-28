d9a0de353bf12b4b5f74b5eec0f24e1877799f5d
Marwa Ibrahim <k0826719@student.kirkwood.edu>
2022-10-28 12:21:25 -0500

    Syncing project

---
+++ tests/codegym/OurLinkedListTest.java
@@ -0,0 +1,47 @@
+package codegym;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class OurLinkedListTest {
+
+    @Test
+    void add() {
+        OurLinkedList<String> list = new OurLinkedList<>();
+        list.add("pizza");
+        list.add("chicken wings");
+        list.add("french fries");
+        assertEquals("pizza", list.get(0));
+        assertEquals("chicken wings", list.get(1));
+        assertEquals("french fries", list.get(2));
+    }
+
+    @Test
+    void testAdd() {
+    }
+
+    @Test
+    void testPop() {
+        OurLinkedList<String> list = new OurLinkedList<>();
+        list.add("pizza");
+        list.add("chicken wings");
+        list.add("french fries");
+        assertEquals("french fries", list.pop());
+    }
+    @Test
+    void testRemove() {
+        OurLinkedList<String> list = new OurLinkedList<>();
+        list.add("pizza");
+        list.add("chicken wings");
+        list.add("french fries");
+        String remove1 = list.remove();
+        // first head remove is null because add method never set value for first.
+        assertNull(remove1);
+        String remove2 = list.remove();
+        // now we can expect the first node added to be return.
+        assertEquals(remove2, "pizza");
+
+    }
+}
