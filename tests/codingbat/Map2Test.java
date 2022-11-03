d246c93e5a844111f07d0548949846d42f969254
Anthoney Hale <ahale06@aim.com>
2022-11-03 17:29:17 -0500

    Map2Test

--- tests/codingbat/Map2Test.java
+++ tests/codingbat/Map2Test.java
@@ -1,14 +1,14 @@
 package codingbat;
 
-import codingbat.Map2;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class Map2Test {
-    
+
     @Test
     void marc_allSwap() {
         String[] strings = {"cat", "dog", "bunny", "caterpillar", "kangaroo", "kitten", "butterfly", "crocodile", "centipede"};
@@ -17,4 +17,12 @@ class Map2Test {
         assertEquals(Arrays.toString(expected), Arrays.toString(result));
     }
 
+    @Test
+    void toney_word0() {
+        String[] map = {"a", String.valueOf(1),"b", String.valueOf(2)};
+
+        Map<String, Integer> results = Map2.word0(map);
+        assertTrue(results.containsKey("a"));
+        assertFalse(results.containsValue(1));
+    }
 }
